05b1c9c39c5325a173b39691a21d7540ef569095
gabrelmega <gabrelmega123123@gmail.com>
2024-09-24 17:59:23 -0300

    Created the base of the calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+const display = document.getElementById("display");
+const numberBtns = document.querySelectorAll(".number")
+
+let number1 = ""
+let number2 = ""
+let operator = ""
+
+
+
+function operate(num1, operator, num2) {
+
+}
+
+function displayFunc() {
+    
+}
