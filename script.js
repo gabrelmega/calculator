80ed155c40c6160e2fb30364e76266d32f6b54c3
gabrelmega <gabrelmega123123@gmail.com>
2024-09-25 19:18:12 -0300

    Display update Now all numberBtns have an Event listener, but just for the display. Added Event listener for operatorBtns, display only. You can't repeat it. added some buttons id's on the HTML. created container div. Created the functions for the operate().

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-const display = document.getElementById("display");
-const numberBtns = document.querySelectorAll(".number")
-
-let number1 = ""
-let number2 = ""
-let operator = ""
-
-
-
-function operate(num1, operator, num2) {
-
+function add(a, b) {
+    return a + b;
 }
 
-function displayFunc() {
-    
+function substraction(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const display = document.querySelector("#display");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+const equals = document.querySelector("#result")
+
+let number1 = "";
+let number2 = "";
+let operator = null;
+
+numberBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        display.textContent += e.target.textContent;
+    });
+});
+
+operatorBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if(operator === null) {
+            operator = e.target.textContent
+            display.textContent += e.target.textContent;
+        } else {
+            operate()
+        }
+    });
+});
+
+function operate() {
+    const firstNumber = number1;
+    const secondNumber = number2;
+    const sign = operator;
+    const result = ""
+    if (sign == "+") {
+        add(firstNumber, secondNumber);
+    } else if (sign == "-") {
+        substraction(firstNumber, secondNumber);
+    } else if (sign == "*") {
+        multiply(firstNumber, secondNumber);
+    } else if(sign == "/") {
+        divide(firstNumber, secondNumber);
+    }
 }
