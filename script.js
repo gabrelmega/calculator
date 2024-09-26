4ea7263a875aa025e12047f61fba704512e6e09f
gabrelmega <gabrelmega123123@gmail.com>
2024-09-26 17:52:19 -0300

    IT WORKS!!! update operatorBtns and numberBtns. Statement on number2 still an placeholder added the style file. Nothing inside for now. added a initial 0 on the display. equals is now working, and you can't put more than one operator. updated function operate().

--- script.js
+++ script.js
@@ -1,57 +1,76 @@
-function add(a, b) {
-    return a + b;
-}
+const add = (a, b) => a + b;
 
-function substraction(a, b) {
-    return a - b;
-}
+const substraction = (a, b) => a - b;
 
-function multiply(a, b) {
-    return a * b;
-}
+const multiply = (a, b) => a * b;
 
-function divide(a, b) {
-    return a / b;
-}
+const divide = (a, b) => b == 0 ? "ERROR" : a / b;
 
 const display = document.querySelector("#display");
 const numberBtns = document.querySelectorAll(".number");
 const operatorBtns = document.querySelectorAll(".operator");
-const equals = document.querySelector("#result")
+const equals = document.querySelector("#result");
 
-let number1 = "";
+let number1 = 0;
 let number2 = "";
 let operator = null;
+let result = 0;
+
+display.textContent = "0"
 
 numberBtns.forEach((button) => {
     button.addEventListener("click", (e) => {
-        display.textContent += e.target.textContent;
+        if (operator === null) {
+            if (number1 == result) {
+                display.textContent = e.target.textContent;
+                number1 = e.target.textContent;
+            } else {
+                display.textContent += e.target.textContent;
+                number1 += e.target.textContent;
+            }
+        } else if (operator != null) {
+            display.textContent += e.target.textContent;
+            number2 += e.target.textContent;
+        } 
     });
 });
 
 operatorBtns.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if(operator === null) {
-            operator = e.target.textContent
+        if(operator === null && number2 === "") {
+            operator = e.target.textContent;
             display.textContent += e.target.textContent;
-        } else {
-            operate()
+        } else if (operator != null && number2 == "0123456789") {
+            operate();
+            display.textContent = result;
+            operator = null;
+            number1 = result;
+            number2 = "";
+            operator = e.target.textContent;
+            display.textContent += e.target.textContent;
         }
     });
 });
 
+equals.addEventListener("click",() => {
+    operate();
+    display.textContent = result;
+    operator = null;
+    number1 = result;
+    number2 = "";
+});
+
 function operate() {
-    const firstNumber = number1;
-    const secondNumber = number2;
-    const sign = operator;
-    const result = ""
-    if (sign == "+") {
-        add(firstNumber, secondNumber);
-    } else if (sign == "-") {
-        substraction(firstNumber, secondNumber);
-    } else if (sign == "*") {
-        multiply(firstNumber, secondNumber);
-    } else if(sign == "/") {
-        divide(firstNumber, secondNumber);
+    firstNumber = parseInt(number1);
+    secondNumber = parseInt(number2);
+    operating = operator
+    if (operating == "+") {
+        result = add(firstNumber, secondNumber);
+    } else if (operating == "-") {
+        result = substraction(firstNumber, secondNumber);
+    } else if (operating == "*") {
+        result = multiply(firstNumber, secondNumber);
+    } else if(operating == "/") {
+        result = divide(firstNumber, secondNumber);
     }
 }
